4b4ed4e4df0fe644050a6f6436f03fc8fdc5d9d0
stormdony <stormdony@foxmail.com>
2019-03-05 19:25:13 +0800

    Site updated: 2019-03-05 19:24:58

--- js/src/dytitle.js
+++ js/src/dytitle.js
@@ -1,17 +1,3 @@
-<!--动态标题-->
-var OriginTitile = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/img/TEP.ico");
-        document.title = ' 你不理我了！';
-        clearTimeout(titleTime);
-    }
-    else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
-        document.title = ' 么么哒 ' + OriginTitile;
-        titleTime = setTimeout(function () {
-            document.title = OriginTitile;
-        }, 2000);
-    }
-});
+// build time:Tue Mar 05 2019 19:23:14 GMT+0800 (GMT+08:00)
+var OriginTitile=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title=" 你不理我了！";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title=" 么么哒 "+OriginTitile;titleTime=setTimeout(function(){document.title=OriginTitile},2e3)}});
+//rebuild by neat 
