218f93f37900abeb8f767694bf15b192f699f6b3
stormdony <stormdony@foxmail.com>
2019-04-25 10:46:54 +0800

    Site updated: 2019-04-25 10:46:27

--- js/src/dytitle.js
+++ js/src/dytitle.js
@@ -1,3 +1,3 @@
-// build time:Mon Apr 22 2019 16:42:41 GMT+0800 (GMT+08:00)
+// build time:Thu Apr 25 2019 10:39:32 GMT+0800 (GMT+08:00)
 var OriginTitile=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title=" 你不理我了！";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title=" 么么哒 "+OriginTitile;titleTime=setTimeout(function(){document.title=OriginTitile},2e3)}});
 //rebuild by neat 
