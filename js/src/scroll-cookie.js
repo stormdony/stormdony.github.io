ff88d3861b04b2c52b4b5c8fbeb91c0a6cd05f0d
stormdony <stormdony@foxmail.com>
2019-07-23 10:33:02 +0800

    Site updated: 2019-07-23 10:32:43

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Jul 05 2019 11:09:31 GMT+0800 (GMT+08:00)
+// build time:Tue Jul 23 2019 10:32:20 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
