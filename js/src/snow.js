d5744b618990c1c3b2ca28c1fe81ad293c1c4e03
stormdony <stormdony@foxmail.com>
2019-08-07 09:05:25 +0800

    Site updated: 2019-08-07 09:05:11

--- js/src/snow.js
+++ js/src/snow.js
@@ -1,3 +1,3 @@
-// build time:Wed Aug 07 2019 09:01:51 GMT+0800 (GMT+08:00)
+// build time:Wed Aug 07 2019 09:04:57 GMT+0800 (GMT+08:00)
 (function(n){n.fn.snow=function(o){var e=n('<div id="snowbox" />').css({position:"absolute","z-index":"9999",top:"-50px"}).html("&#10052;"),t=n(document).height(),i=n(document).width(),a={minSize:10,maxSize:20,newOn:1e3,flakeColor:"#AFDAEF"},o=n.extend({},a,o);var m=setInterval(function(){var a=Math.random()*i-100,m=.5+Math.random(),r=o.minSize+Math.random()*o.maxSize,c=t,d=a,f=t*10+Math.random()*5e3;e.clone().appendTo("body").css({left:a,opacity:m,"font-size":r,color:o.flakeColor}).animate({top:c,left:d,opacity:.2},f,"linear",function(){n(this).remove()})},o.newOn)}})(jQuery);$(function(){$.fn.snow({minSize:5,maxSize:50,newOn:200})});
 //rebuild by neat 
